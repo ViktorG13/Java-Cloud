155fa529127d6fd5e8e879301a5208e36cce4a56
ViktorG13 <eduardoviktor0@outlook.com>
2026-08-15 22:56:44 -0300

    :sparkles: feat: Started the front and page structure

--- src/main/java/com/redblack/extension/delivery_manager/controllers/DeliveriesController.java
+++ src/main/java/com/redblack/extension/delivery_manager/controllers/DeliveriesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DeliveriesController {
-    @GetMapping("/deliveries")
+    @GetMapping("/")
     public String overview(Model model) {
         return "deliveries";
     }
