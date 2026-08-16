9f09910edaff3f05cfdcd9e2ff2ac18505a53c7c
ViktorG13 <eduardoviktor0@outlook.com>
2026-08-15 23:02:30 -0300

    :sparkles: feat: Started the front and page structure

--- src/main/java/com/redblack/extension/delivery_manager/controllers/DeliveriesController.java
+++ src/main/java/com/redblack/extension/delivery_manager/controllers/DeliveriesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DeliveriesController {
-    @GetMapping("/")
+    @GetMapping("/deliveries")
     public String overview(Model model) {
         return "deliveries";
     }
--- src/main/java/com/redblack/extension/delivery_manager/controllers/HomeController.java
+++ src/main/java/com/redblack/extension/delivery_manager/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/dashboard")
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("user", "User");
         return "dashboard";
